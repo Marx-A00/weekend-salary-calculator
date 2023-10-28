11b0b6e86174ec4657e267df167f8a980e3f9aa1
Marcos Andrade <mnandrade1999@gmail.com>
2023-10-28 14:42:35 -0500

    added main functionality to table including adding rows for each employee and updating total monthly cost

--- INSTRUCTIONS.md
+++ INSTRUCTIONS.md
@@ -4,12 +4,12 @@ Your task is to create an application that records employee salaries and adds sa
 
 ## Requirements:
 
-* This application should have a form with five inputs that collect a new employee's *first name, last name, ID number, job title, annual salary*.
+x This application should have a form with five inputs that collect a new employee's *first name, last name, ID number, job title, annual salary*.
 
 * When the "Submit" button is clicked:
-    * A new employee row should be added to the table.
+    x A new employee row should be added to the table.
     * The footer's total monthly cost should be updated.
-    * The form inputs should be cleared out.
+    x The form inputs should be cleared out.
 
 * If the total monthly cost **exceeds $20,000**, apply an `over-budget` CSS class to the footer element.
     * When applied, this CSS class should provide a clear visual indication that the monthly cost has been exceeded. (This could be as simple as turning the footer text red.)
--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+// handles total monthly Salary displayed in footer 
+let totalMonthly = 0;
+
 function handleSubmit(event){
     event.preventDefault();
+// Get *value* from HTML and set it to empty after submission
+    let firstName = document.getElementById(`firstName`).value;
+    document.getElementById(`firstName`).value = '';
+
+// Get *value* from HTML and set it to empty after submission
+    let lastName = document.getElementById(`lastName`).value;
+    document.getElementById(`lastName`).value = '';
+
+// Get *value* from HTML and set it to empty after submission
+    let id = document.getElementById(`id`).value;
+    document.getElementById(`id`).value = '';
+
+// Get *value* from HTML and set it to empty after submission
+    let title = document.getElementById(`title`).value;
+    document.getElementById(`title`).value = '';
+
+// Get *value* from HTML and set it to empty after submission
+    let annualSalary = Number(document.getElementById(`annualSalary`).value);
+
+    // Update total Salary
+    totalMonthly += Math.ceil(annualSalary/12); // Might fuck with the tests        Remember this!!
+
+    console.log(totalMonthly);
+    document.getElementById(`annualSalary`).value ='';
+
+    tableBody.innerHTML +=
+    `<tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${id}</td>
+        <td>${title}</td>
+        <td>$${annualSalary}</td>      
+        
+    </tr>`;
+    document.getElementById(`footy`).textContent = `Total Monthy: ${totalMonthly}`;
 }
