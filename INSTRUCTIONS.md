3cda7c04c72ddadbc93345679ab59306bb0665c8
Marcos Andrade <mnandrade1999@gmail.com>
2023-10-29 17:13:49 -0500

    base functionality complete, stretch goals not attempted due to struggles with test

--- INSTRUCTIONS.md
+++ INSTRUCTIONS.md
@@ -6,15 +6,15 @@ Your task is to create an application that records employee salaries and adds sa
 
 x This application should have a form with five inputs that collect a new employee's *first name, last name, ID number, job title, annual salary*.
 
-* When the "Submit" button is clicked:
+x When the "Submit" button is clicked:
     x A new employee row should be added to the table.
-    * The footer's total monthly cost should be updated.
+    x The footer's total monthly cost should be updated.
     x The form inputs should be cleared out.
 
-* If the total monthly cost **exceeds $20,000**, apply an `over-budget` CSS class to the footer element.
-    * When applied, this CSS class should provide a clear visual indication that the monthly cost has been exceeded. (This could be as simple as turning the footer text red.)
+x If the total monthly cost **exceeds $20,000**, apply an `over-budget` CSS class to the footer element.
+    x When applied, this CSS class should provide a clear visual indication that the monthly cost has been exceeded. (This could be as simple as turning the footer text red.)
 
-* Create a "Delete" button that removes an employee from the DOM.
+x Create a "Delete" button that removes an employee from the DOM.
     * **For base mode**, the total monthly cost **does not** need to be updated when an employee is deleted.
 
 ## Please Don't Fork This Repo:
--- script.js
+++ script.js
@@ -5,28 +5,34 @@ function handleSubmit(event){
     event.preventDefault();
 // Get *value* from HTML and set it to empty after submission
     let firstName = document.getElementById(`firstName`).value;
-    document.getElementById(`firstName`).value = '';
+    document.getElementById(`firstName`).value= "";
 
 // Get *value* from HTML and set it to empty after submission
     let lastName = document.getElementById(`lastName`).value;
-    document.getElementById(`lastName`).value = '';
+    document.getElementById(`lastName`).value = "";
 
 // Get *value* from HTML and set it to empty after submission
     let id = document.getElementById(`id`).value;
-    document.getElementById(`id`).value = '';
+    document.getElementById(`id`).value = "";
 
 // Get *value* from HTML and set it to empty after submission
     let title = document.getElementById(`title`).value;
-    document.getElementById(`title`).value = '';
+    document.getElementById(`title`).value = "";
 
 // Get *value* from HTML and set it to empty after submission
     let annualSalary = Number(document.getElementById(`annualSalary`).value);
 
     // Update total Salary
-    totalMonthly += Math.ceil(annualSalary/12); // Might fuck with the tests        Remember this!!
+    totalMonthly += (annualSalary/12); // Might fuck with the tests        Remember this!!
+    
+    // handles addition of overBudget class to footer once total monthly cost exceeds 20000
+    if(totalMonthly > 20000){
+        let footer = document.querySelector("footer");
+        footer.classList.add("over-budget");
+    }
 
     console.log(totalMonthly);
-    document.getElementById(`annualSalary`).value ='';
+    document.getElementById(`annualSalary`).value = "";
 
     tableBody.innerHTML +=
     `<tr>
@@ -34,8 +40,13 @@ function handleSubmit(event){
         <td>${lastName}</td>
         <td>${id}</td>
         <td>${title}</td>
-        <td>$${annualSalary}</td>      
+        <td>$${annualSalary}</td>
+        <td onclick="deleteRow(event)"><button>Ⓧ</button></td>      
         
     </tr>`;
-    document.getElementById(`footy`).textContent = `Total Monthy: ${totalMonthly}`;
+    document.getElementById(`footy`).innerHTML = `Total Monthy: ${totalMonthly}`;
+}
+function deleteRow(event){
+    event.target.parentElement.parentElement.remove();
+
 }
